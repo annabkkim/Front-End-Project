a8d90d2f0a6ef2200a649be79785e61c83bdd82d
Anna Kim <AnnaKim@Rosas-Air.attlocal.net>
2020-07-29 16:22:37 -0400

    custom javascript

--- app.js
+++ app.js
@@ -26,3 +26,12 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+
+// button for subscription
+
+document.querySelectorAll('.btn').forEach(function (a) {
+    a.addEventListener('click', function(){
+        this.style.backgroundColor= 'pink';
+    })
+});
